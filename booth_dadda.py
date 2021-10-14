46743672f48c872645c77d68c4027068d2d86033
Anton Blanchard <anton@ozlabs.org>
2021-10-14 20:50:22 +1100

    Still not working

--- booth_dadda.py
+++ booth_dadda.py
@@ -127,32 +127,28 @@ class BoothDadda(Elaboratable):
 
             # Step through the multiplicand 1 bit at a time
             for off_m in range(self.bits+1):
-                mand = multiplicand[off_m:off_m+2]
+                #mand = multiplicand[off_m:off_m+2]
 
                 o = Signal()
                 partial_products[off_b + off_m].append(o)
 
                 booth = BoothRadix4()
-                m.submodules += booth
+                name = "booth_b%d_m%d" % (off_b, off_m)
+                m.submodules[name] = booth
 
                 m.d.comb += [
                     booth.block.eq(block),
-                    booth.multiplicand.eq(mand),
+                    booth.multiplicand.eq(multiplicand[off_m:off_m+2]),
                     o.eq(booth.o),
                  ]
 
         # Dadda reduction
         dadda_heights = self._calc_dadda_heights(self.bits//2)
 
+        iteration = 0
+
         # Loop until we have a depth of 2
         while max(len(x) for x in partial_products) > 2:
-            print(max(len(x) for x in partial_products))
-            print(dadda_heights)
-
-            #for offset in range(len(partial_products)):
-                #print("%d" % len(partial_products[offset]), end='')
-            #print()
-
             for offset in range(len(partial_products)):
                 while len(partial_products[offset]) > dadda_heights[0]:
                     s = Signal()
@@ -161,7 +157,8 @@ class BoothDadda(Elaboratable):
                     # Full adder of three bits if there are 2 or more extra elements
                     if len(partial_products[offset]) > (1 + dadda_heights[0]):
                         fa = FullAdder()
-                        m.submodules += fa
+                        name = "dadda_fa_%d_%d" % (iteration, offset)
+                        m.submodules[name] = fa
 
                         i0 = partial_products[offset].pop(0)
                         i1 = partial_products[offset].pop(0)
@@ -172,14 +169,15 @@ class BoothDadda(Elaboratable):
                             fa.b.eq(i1),
                             fa.carry_in.eq(i2),
 
-                            fa.sum.eq(s),
-                            fa.carry_out.eq(c),
+                            s.eq(fa.sum),
+                            c.eq(fa.carry_out),
                         ]
 
                     # Half adder of two bits if there is 1 extra element
                     else:
                         ha = HalfAdder()
-                        m.submodules += ha
+                        name = "dadda_ha_%d_%d" % (iteration, offset)
+                        m.submodules[name] = ha
 
                         i0 = partial_products[offset].pop(0)
                         i1 = partial_products[offset].pop(0)
@@ -188,8 +186,8 @@ class BoothDadda(Elaboratable):
                             ha.a.eq(i0),
                             ha.b.eq(i1),
 
-                            ha.sum.eq(s),
-                            ha.carry_out.eq(c),
+                            s.eq(ha.sum),
+                            c.eq(ha.carry_out),
                         ]
 
                     # result goes in bottom of column and carry goes in the top of the next column
@@ -197,22 +195,14 @@ class BoothDadda(Elaboratable):
                     partial_products[offset+1].insert(0, c)
 
             dadda_heights.pop(0)
+            iteration = iteration + 1
 
         for offset in range(len(partial_products)):
-            # Why
-            if len(partial_products[offset]) == 0:
-                partial_products[offset].append(Const(0))
-                partial_products[offset].append(Const(0))
-
-            elif len(partial_products[offset]) == 1:
+            while len(partial_products[offset]) < 2:
                 partial_products[offset].append(Const(0))
 
         r1 = Cat(partial_products[n][0] for n in range(len(partial_products)))
         r2 = Cat(partial_products[n][1] for n in range(len(partial_products)))
-
-        #print(partial_products)
-        #print(r1)
-        #print(r2)
 
         m.d.comb += self.o.eq(r1+r2)
 
@@ -223,3 +213,62 @@ if __name__ == "__main__":
     top = BoothDadda(bits=4)
     with open("booth.v", "w") as f:
         f.write(verilog.convert(top, ports = [top.a, top.b, top.o], strip_internal_attrs=True))
+
+cases = [
+        0x0000000000000000,
+        0x0000000000000001,
+        0x0000000011111111,
+        0x000000007fffffff,
+        0x0000000080000000,
+        0x00000000ffffffff,
+        0x0000000100000000,
+        0x0001020304050607,
+        0x1111111111111111,
+        0x7fffffffffffffff,
+        0x8000000000000000,
+        0x8888888888888888,
+        0xffffffff00000000,
+        0xffffffffffffffff,
+        0X00ff00ff00ff00ff,
+        0Xff00ff00ff00ff00,
+        0xa5a5a5a5a5a5a5a5,
+]
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.dut = BoothDadda(4)
+
+    def do_one_comb(self, a, b):
+        yield self.dut.a.eq(a)
+        yield self.dut.b.eq(b)
+        yield Settle()
+        res = (yield self.dut.o)
+        self.assertEqual(res, a * b)
+
+#    def test_cases(self):
+#        def bench():
+#            for (a, b) in [(x, y) for x in cases for y in cases]:
+#                print("a %x b %x" % (a, b))
+#                rand_a = random.getrandbits(64)
+#                rand_b = random.getrandbits(64)
+#                yield from self.do_one_comb(rand_a, rand_b)
+#
+#        sim = Simulator(self.dut)
+#        sim.add_process(bench)
+#        with sim.write_vcd("test.vcd"):
+#            sim.run()
+
+    def test_random(self):
+        def bench():
+            for i in range(1000):
+                rand_a = 2
+                rand_b = 1
+                yield from self.do_one_comb(rand_a, rand_b)
+
+        sim = Simulator(self.dut)
+        sim.add_process(bench)
+        with sim.write_vcd("test.vcd"):
+            sim.run()
+
+if __name__ == '__main__':
+    unittest.main()
