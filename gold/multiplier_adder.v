3440d9a85cc104290933da700ec62fb45f495a0b
Anton Blanchard <anton@ozlabs.org>
2021-10-19 19:17:56 +1100

    Add gold/multiplier_adder.v

---
+++ gold/multiplier_adder.v
@@ -0,0 +1,11 @@
+module gold_multiplier_adder
+#(
+    parameter BITS=64
+) (
+    input [BITS-1:0] a,
+    input [BITS-1:0] b,
+    input [BITS*2-1:0] c,
+    output [BITS*2-1:0] o
+);
+    assign o = a * b + c;
+endmodule
