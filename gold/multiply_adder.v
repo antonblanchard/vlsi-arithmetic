100585aa17c7e7fe5976189d8c75478972c36e1f
Anton Blanchard <anton@ozlabs.org>
2022-03-18 15:52:56 +1100

    Add conditional power pins to gold models

--- gold/multiply_adder.v
+++ gold/multiply_adder.v
@@ -2,6 +2,10 @@ module gold_multiply_adder
 #(
     parameter BITS=64
 ) (
+`ifdef USE_POWER_PINS
+    input VPWR,
+    input VGND,
+`endif
     input [BITS-1:0] a,
     input [BITS-1:0] b,
     input [BITS*2-1:0] c,
