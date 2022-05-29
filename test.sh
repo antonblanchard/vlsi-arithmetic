df50863da6348fb480fb06cf75480ba7fd8c447c
Anton Blanchard <anton@ozlabs.org>
2022-05-29 11:47:25 +1000

    Update tests

--- test.sh
+++ test.sh
@@ -2,8 +2,36 @@
 
 export PATH=/home/anton_unencrypted/yosys.install/bin/:/home/anton_unencrypted/verilator.install/bin:$PATH
 
+python multiplier.py --bits=8 --multiply-add --process=sky130 --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=0" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
+python multiplier.py --bits=8 --multiply-add --process=sky130 --register-input --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=1" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
+python multiplier.py --bits=8 --multiply-add --process=sky130 --register-middle --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=1" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
+python multiplier.py --bits=8 --multiply-add --process=sky130 --register-output --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=1" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
+python multiplier.py --bits=8 --multiply-add --process=sky130 --register-input --register-middle --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=2" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
+python multiplier.py --bits=8 --multiply-add --process=sky130 --register-input --register-output --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=2" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
+python multiplier.py --bits=8 --multiply-add --process=sky130 --register-middle --register-output --output=generated/multiplier_sky130_16.v
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=2" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+obj_dir/multiplier-verilator
+
 python multiplier.py --bits=8 --multiply-add --process=sky130 --register-input --register-middle --register-output --output=generated/multiplier_sky130_16.v
-verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS -O3 --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
+verilator -O3 -Wno-fatal -Wno-TIMESCALEMOD -CFLAGS "-O3 -DPIPELINE_DEPTH=3" --assert --cc --exe --build generated/multiplier_sky130_16.v sky130/sky130_fd_sc_hd_cutdown.v verilator/multiplier.cpp -o multiplier-verilator -top-module multiply_adder
 obj_dir/multiplier-verilator
 
 python adder.py --bits=64 --process=sky130 --output=generated/adder_sky130.v
@@ -18,4 +46,4 @@ yosys formal/multiply_adder.ys
 python multiplier.py --bits=4 --multiply-add --process=sky130 --register-input --register-middle --register-output --output=generated/multiply_adder_pipelined_sky130.v
 yosys formal/multiply_adder_pipelined.ys
 
-#python -m unittest
+python -m unittest
