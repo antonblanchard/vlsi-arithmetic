023c7a4b175d0e8ea72adaaecf971812e3135b0d
Anton Blanchard <anton@ozlabs.org>
2022-06-07 10:28:28 +1000

    Add Han Carlson unit test

--- tests/test_adder.py
+++ tests/test_adder.py
@@ -3,7 +3,7 @@ import random
 from amaranth.sim import Simulator, Settle
 
 from process_none import ProcessNone
-from adder import BrentKung, KoggeStone
+from adder import BrentKung, KoggeStone, HanCarlson
 
 
 class TestBrentKungAdder(BrentKung, ProcessNone):
@@ -11,6 +11,10 @@ class TestBrentKungAdder(BrentKung, ProcessNone):
 
 
 class TestKoggeStoneAdder(KoggeStone, ProcessNone):
+    pass
+
+
+class TestHanCarlsonAdder(HanCarlson, ProcessNone):
     pass
 
 
@@ -44,6 +48,32 @@ class TestCaseKoggeStoneRandom(unittest.TestCase):
     def setUp(self):
         self.bits = 64
         self.dut = TestKoggeStoneAdder(self.bits)
+
+    def do_one_comb(self, a, b):
+        yield self.dut.a.eq(a)
+        yield self.dut.b.eq(b)
+        yield Settle()
+        res = (yield self.dut.o)
+        expected = (a + b) & (pow(2, self.bits) - 1)
+        self.assertEqual(res, expected)
+
+    def test_random(self):
+        def bench():
+            for i in range(1000):
+                rand_a = random.getrandbits(self.bits)
+                rand_b = random.getrandbits(self.bits)
+                yield from self.do_one_comb(rand_a, rand_b)
+
+        sim = Simulator(self.dut)
+        sim.add_process(bench)
+        with sim.write_vcd("adder_random.vcd"):
+            sim.run()
+
+
+class TestCaseHanCarlsonRandom(unittest.TestCase):
+    def setUp(self):
+        self.bits = 64
+        self.dut = TestHanCarlsonAdder(self.bits)
 
     def do_one_comb(self, a, b):
         yield self.dut.a.eq(a)
